6af1ca35b1e117895b51b90968eda6ed6de11cfe
kitagawakaede <kaede10084203@gmail.com>
2025-02-06 20:10:04 +0900

    playwrightを使えるようにした

--- apps/nestjs/.eslintrc.js
+++ apps/nestjs/.eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist'],
+  ignorePatterns: ['.eslintrc.js', 'dist', 'tests', 'playwright.config.ts', 'playwright-report'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
