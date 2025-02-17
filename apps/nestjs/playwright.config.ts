6343f23d9eb69f17505c96eeba1a46746c9de4c3
kitagawakaede <kaede10084203@gmail.com>
2025-02-17 20:53:02 +0900

    playwrightを元に戻した

--- apps/nestjs/playwright.config.ts
+++ apps/nestjs/playwright.config.ts
@@ -1,11 +1,15 @@
 /* eslint-disable */
 
 import { defineConfig, devices } from "@playwright/test";
-import dotenv from "dotenv";
-import path from "path";
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 // Load .env file
-dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
