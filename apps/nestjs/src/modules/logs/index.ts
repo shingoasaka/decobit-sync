2c087d68721a236c3369bf4fbbd678fbd3d98108
SuenagaKeiya <keiya04201@gmail.com>
2025-02-17 16:04:06 +0900

    webantennaのデータをDBに保存できるようにした

--- apps/nestjs/src/modules/logs/index.ts
+++ apps/nestjs/src/modules/logs/index.ts
@@ -1,3 +1,5 @@
 export { MetronClickLogsService } from "../metron/click-logs.service";
 export { MetronActionLogsService } from "../metron/action-logs.service";
 export { TryActionLogService } from "../hanikamu/try/action-logs.service";
+export { WebantennaActionLogService } from "../webantenna/mens-tbc-and-tbc/action-logs.service";
+export { WebantennaClickLogService } from "../webantenna/mens-tbc-and-tbc/click-logs.service";
