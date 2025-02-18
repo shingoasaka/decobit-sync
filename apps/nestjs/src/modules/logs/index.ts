45c57515ffc5c0fcbaec5af50edd64f0b6b6d08d
kitagawakaede <kaede10084203@gmail.com>
2025-02-18 13:07:11 +0900

    AUR-4：quoriza(メンズクリアとストラッシュ)のデータを取得できるようにした

--- apps/nestjs/src/modules/logs/index.ts
+++ apps/nestjs/src/modules/logs/index.ts
@@ -3,3 +3,5 @@ export { MetronActionLogsService } from "../metron/action-logs.service";
 export { TryActionLogService } from "../hanikamu/try/action-logs.service";
 export { NavicluActionLogService } from "../rentracks/naviclu/action-log.service";
 export { NavicluClickLogService } from "../rentracks/naviclu/click-log.service";
+export {MensclearActionLogService} from "../quoriza/mensclear/action-log.service";
+export {StlasshActionLogService} from "../quoriza/stlassh/action-log.service"
--- apps/nestjs/src/modules/repositories/index.ts
+++ apps/nestjs/src/modules/repositories/index.ts
@@ -1,4 +1,6 @@
 export { TryActionLogRepository } from "../hanikamu/try/action-logs.repository";
 export { NavicluActionLogRepository } from "../rentracks/naviclu/action-log.repository";
 export { NavicluClickLogRepository } from "../rentracks/naviclu/click-log.repository";
+export {MensclearActionLogRepository} from "../quoriza/mensclear/action-log.repository";
+export {StlasshActionLogRepository} from "../quoriza/stlassh/action-log.repository"
 // 今後追加されるレポジトリもここでエクスポート
