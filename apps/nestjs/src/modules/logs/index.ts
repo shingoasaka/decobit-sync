844792ef6b025ef2e5b55040b04b86abb9352164
kitagawakaede <kaede10084203@gmail.com>
2025-04-09 19:58:26 +0900

    クオリザのスクレイピング処理、スキーマを削除した

--- apps/nestjs/src/modules/logs/index.ts
+++ apps/nestjs/src/modules/logs/index.ts
@@ -1,6 +1,2 @@
-export { MensclearActionLogService } from "../asp/quoriza/mensclear/action-logs.service";
-export { MensclearClickLogService } from "../asp/quoriza/mensclear/click-logs.service";
 export { SparkOripaActionLogService } from "../asp/adebis/spark-oripa/action-logs.service";
 export { SparkOripaClickLogService } from "../asp/adebis/spark-oripa/click-logs.service";
-export { StlasshActionLogService } from "../asp/quoriza/stlassh/action-logs.service";
-export { StlasshClickLogService } from "../asp/quoriza/stlassh/click-logs.service";
