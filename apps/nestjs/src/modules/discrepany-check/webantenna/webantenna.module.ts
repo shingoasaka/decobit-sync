8fbedf65f9a90c02d8d4a93f3395e15418108a4c
kitagawakaede <kaede10084203@gmail.com>
2025-04-22 13:25:36 +0900

    webantenna-clickを削除した

--- apps/nestjs/src/modules/discrepany-check/webantenna/webantenna.module.ts
+++ apps/nestjs/src/modules/discrepany-check/webantenna/webantenna.module.ts
@@ -1,18 +1,14 @@
 import { Module } from "@nestjs/common";
 import { WebantennaActionLogService } from "./services/action-logs.service";
-import { WebantennaClickLogService } from "./services/click-logs.service";
 import { WebantennaActionLogRepository } from "./repositories/action-logs.repository";
-import { WebantennaClickLogRepository } from "./repositories/click-logs.repository";
 import { PrismaService } from "@prismaService";
 
 @Module({
   providers: [
     WebantennaActionLogService,
-    WebantennaClickLogService,
     WebantennaActionLogRepository,
-    WebantennaClickLogRepository,
     PrismaService,
   ],
-  exports: [WebantennaActionLogService, WebantennaClickLogService],
+  exports: [WebantennaActionLogService],
 })
 export class WebantennaModule {}
