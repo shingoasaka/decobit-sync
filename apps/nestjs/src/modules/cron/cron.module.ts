1e3a3d70f4e32ac31aeb7306c7f56ae273ceff48
Rem Araki <arakirem2001@gmail.com>
2025-06-09 12:14:16 +0900

    fix:comment_out

--- apps/nestjs/src/modules/cron/cron.module.ts
+++ apps/nestjs/src/modules/cron/cron.module.ts
@@ -1,21 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
-// import { AspCronService } from "./services/asp-cron.service";
+import { AspCronService } from "./services/asp-cron.service";
 import { MediaCronService } from "./services/media-cron.service";
-// import { AspModule } from "@asp/asp.module";
+import { AspModule } from "@asp/asp.module";
 import { CommonLogService } from "@logs/common-log.service";
 import { MediaModule } from "../media/media.module";
 
 @Module({
-  imports: [
-    ScheduleModule.forRoot(),
-    // AspModule,
-    MediaModule,
-  ],
-  providers: [
-    // AspCronService,
-    CommonLogService,
-    MediaCronService,
-  ],
+  imports: [ScheduleModule.forRoot(), AspModule, MediaModule],
+  providers: [AspCronService, CommonLogService, MediaCronService],
 })
 export class CronModule {}
